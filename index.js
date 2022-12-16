5ee15ef1bd4897c666d22e703cf1dd31516465e9
Kogby <kogby0507@gmail.com>
2022-12-17 03:57:16 +0800

    connect db with ngrok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import mysql from "mysql";
 
 dotenv.config(); //這樣才能讀.env檔案
 
@@ -19,6 +20,22 @@ app.get("/", (req, res) => {
     return res.status(200).send("The server is running.");
 });
 
+var db = mysql.createConnection({
+  host: "0.tcp.jp.ngrok.io",
+  port: "11377",
+  user: "root",
+  password: "123456",
+  database: "shoppingDB"
+})
+
+db.connect(function(err) {
+  if (err) throw err;
+  db.query("SHOW tables", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+  console.log("MySQL connected. ")
+})
 
 // The server is now listeing
 app.listen(port, ()=>{
